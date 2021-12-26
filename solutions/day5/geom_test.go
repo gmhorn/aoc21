16e226cd332b6872057fcf23de343fe839c6431f
gmhorn <46626789+gmhorn@users.noreply.github.com>
2021-12-25 21:39:00 -0800

    Day5 (#6)
    Day 5

---
+++ solutions/day5/geom_test.go
@@ -0,0 +1,30 @@
+package day5
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLine(t *testing.T) {
+	actual, err := ParseLine("1,2 -> 30,40")
+	assert.NoError(t, err)
+	assert.Equal(t, Line{1, 2, 30, 40}, actual)
+}
+
+func TestCountOverlaps(t *testing.T) {
+	lines := []Line{
+		{0, 9, 5, 9},
+		{8, 0, 0, 8},
+		{9, 4, 3, 4},
+		{2, 2, 2, 1},
+		{7, 0, 7, 4},
+		{6, 4, 2, 0},
+		{0, 9, 2, 9},
+		{3, 4, 1, 4},
+		{0, 0, 8, 8},
+		{5, 5, 8, 2},
+	}
+
+	assert.Equal(t, 12, CountOverlaps(lines, 2))
+}
