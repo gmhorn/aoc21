e549fc3a2a48eb4ed4f45a810336e4bab1ce59b9
Gerald Horn <geraldmhorn@gmail.com>
2021-12-26 12:54:15 -0800

    Part 1

---
+++ solutions/day6/solution_test.go
@@ -0,0 +1,28 @@
+package day6
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestState(t *testing.T) {
+	var init state
+	init[1] = 1
+	init[2] = 1
+	init[3] = 2
+	init[4] = 1
+
+	day1 := nextState(init)
+	assert.Equal(t, state([9]int{1, 1, 2, 1, 0, 0, 0, 0, 0}), day1)
+	day2 := nextState(day1)
+	assert.Equal(t, state([9]int{1, 2, 1, 0, 0, 0, 1, 0, 1}), day2)
+	day3 := nextState(day2)
+	assert.Equal(t, state([9]int{2, 1, 0, 0, 0, 1, 1, 1, 1}), day3)
+
+	next := init
+	for i := 0; i < 18; i++ {
+		next = nextState(next)
+	}
+	assert.Equal(t, 26, sum(next))
+}
