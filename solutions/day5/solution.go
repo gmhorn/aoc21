f02de400384c8536f458bb56823e0b23ff0707be
Gerald Horn <geraldmhorn@gmail.com>
2021-12-25 18:23:55 -0800

    Part 1 working

--- solutions/day5/solution.go
+++ solutions/day5/solution.go
@@ -1,11 +1,30 @@
 package day5
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/gmhorn/aoc21/lib"
+)
 
 type Solution struct{}
 
 func (sln Solution) Part1(input string) (int, error) {
-	return -1, errors.New("not implemented")
+	strs, err := lib.ReadLines(input)
+	if err != nil {
+		return -1, err
+	}
+
+	lines := make([]Line, 0)
+	for idx, str := range strs {
+		line, err := NewLine(str)
+		if err != nil {
+			return -1, fmt.Errorf("could not parse line %d: %v", idx, err)
+		}
+		lines = append(lines, line)
+	}
+
+	return CountOverlaps(lines, 2), nil
 }
 
 func (sln Solution) Part2(input string) (int, error) {
