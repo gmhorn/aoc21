019ccf2d32c1c1d199644d77d0f6d621338691fa
Gerald Horn <geraldmhorn@gmail.com>
2021-12-27 16:39:45 -0800

    Add Signal type

--- solutions/day8/solution.go
+++ solutions/day8/solution.go
@@ -41,38 +41,82 @@ func (sln Solution) Part2(input string) (int, error) {
 	return -1, errors.New("not implemented")
 }
 
-var unscrambled = []Pattern{
-	ToPattern("ABCEFG"),  // 0
-	ToPattern("CF"),      // 1
-	ToPattern("ACDEG"),   // 2
-	ToPattern("ACDFG"),   // 3
-	ToPattern("BCDF"),    // 4
-	ToPattern("ABDFG"),   // 5
-	ToPattern("ABDEFG"),  // 6
-	ToPattern("ACF"),     // 7
-	ToPattern("ABCDEFG"), // 8
-	ToPattern("ABCDFG"),  // 9
+var (
+	alphabetScrambled   = NewSignal("abcdefg")
+	alphabetUnscrambled = NewSignal("ABCDEFG")
+
+	// unscrambled is an array where each (unscrambled) Signal's index
+	// represents that Signal's digit value.
+	unscrambled = []Signal{
+		NewSignal("ABCEFG"),  // 0
+		NewSignal("CF"),      // 1
+		NewSignal("ACDEG"),   // 2
+		NewSignal("ACDFG"),   // 3
+		NewSignal("BCDF"),    // 4
+		NewSignal("ABDFG"),   // 5
+		NewSignal("ABDEFG"),  // 6
+		NewSignal("ACF"),     // 7
+		NewSignal("ABCDEFG"), // 8
+		NewSignal("ABCDFG"),  // 9
+	}
+)
+
+// Signal is a 7-digit output signal. It is represented as a (sorted) list of
+// runes from a designated alphabet.
+type Signal []rune
+
+// NewSignal creates a new Signal from the input string, making sure to sort
+// the consitituent runes lexigraphically.
+func NewSignal(str string) Signal {
+	sig := Signal(str)
+	sort.Slice(sig, func(i int, j int) bool {
+		return sig[i] < sig[j]
+	})
+	return sig
+}
+
+// Exclude constructs a new Signal that consists of this Signal, minus any runes
+// that appear in the other Signal.
+//
+// This relies on both this Signal and the other Signal being lexigraphically
+// sorted and not containing duplicate runes.
+func (s Signal) Exclude(other Signal) Signal {
+	res := make([]rune, 0)
+
+	idxS, idxO := 0, 0
+	for ; idxS < len(s) && idxO < len(other); idxS++ {
+		if s[idxS] == other[idxO] {
+			idxO++
+			continue
+		}
+		res = append(res, s[idxS])
+	}
+
+	res = append(res, s[idxS:]...)
+
+	return res
 }
 
 type Pattern []rune
 
-func (p Pattern) Exclude(other Pattern) Pattern {
-	idx := 0
-	for _, cOther := range other {
-		for idx, cPattern := range p {
-			if cOther == cPattern {
-				break
-			}
-		}
-		if idx < len(pattern)
-	}
-	remaining := make([]rune, 0)
-	for _, c := range p {
+// func (p Pattern) Exclude(other Pattern) Pattern {
+// 	idx := 0
+// 	for _, cOther := range other {
+// 		for idx, cPattern := range p {
+// 			if cOther == cPattern {
+// 				break
+// 			}
+// 		}
+// 		if idx < len(pattern)
+// 	}
+// 	remaining := make([]rune, 0)
+// 	for _, c := range p {
 
-	}
+// 	}
 
-	return remaining
-}
+// 	return remaining
+// }
+
 func ToPattern(str string) Pattern {
 	pat := Pattern(str)
 	sort.Slice(pat, func(i, j int) bool {
@@ -81,30 +125,33 @@ func ToPattern(str string) Pattern {
 	return pat
 }
 
-func process(line string) []Pattern {
+func process(line string) []Signal {
+	// Parse line into list of pattern Signals and list of output Signals
+	// All Signals are using the scrambled alphabet
 	halves := strings.Split(line, "|")
 
-	signals := make([]Pattern, 0)
-	outputs := make([]Pattern, 0)
+	patterns := make([]Signal, 0)
+	outputs := make([]Signal, 0)
 	for _, part := range strings.Split(strings.TrimSpace(halves[0]), " ") {
-		signals = append(signals, ToPattern(part))
+		patterns = append(patterns, NewSignal(part))
 	}
 	for _, part := range strings.Split(strings.TrimSpace(halves[1]), " ") {
-		outputs = append(outputs, ToPattern(part))
+		outputs = append(outputs, NewSignal(part))
 	}
 
-	sort.Slice(signals, func(i, j int) bool {
-		return len(signals[i]) < len(signals[j])
+	// Sort the pattern Signals by length
+	sort.Slice(patterns, func(i, j int) bool {
+		return len(patterns[i]) < len(patterns[j])
 	})
 
-	candidates := map[rune]Pattern{
-		'A': ToPattern("abcdefg"),
-		'B': ToPattern("abcdefg"),
-		'C': ToPattern("abcdefg"),
-		'D': ToPattern("abcdefg"),
-		'E': ToPattern("abcdefg"),
-		'F': ToPattern("abcdefg"),
-		'G': ToPattern("abcdefg"),
-	}
-	return signals
+	// candidates := map[rune]Pattern{
+	// 	'A': ToPattern("abcdefg"),
+	// 	'B': ToPattern("abcdefg"),
+	// 	'C': ToPattern("abcdefg"),
+	// 	'D': ToPattern("abcdefg"),
+	// 	'E': ToPattern("abcdefg"),
+	// 	'F': ToPattern("abcdefg"),
+	// 	'G': ToPattern("abcdefg"),
+	// }
+	return patterns
 }
--- solutions/day8/solution_test.go
+++ solutions/day8/solution_test.go
@@ -1,6 +1,7 @@
 package day8
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,4 +10,38 @@ import (
 func TestIt(t *testing.T) {
 	output := process("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf")
 	assert.NotNil(t, output)
+}
+
+func TestExclude(t *testing.T) {
+	tests := []struct {
+		base, exclude, expected string
+	}{{
+		base:     "abcd",
+		exclude:  "ab",
+		expected: "cd",
+	}, {
+		base:     "abcd",
+		exclude:  "bc",
+		expected: "ad",
+	}, {
+		base:     "abcd",
+		exclude:  "ac",
+		expected: "bd",
+	}, {
+		base:     "abcd",
+		exclude:  "e",
+		expected: "abcd",
+	}}
+
+	for idx, tt := range tests {
+		t.Run(fmt.Sprintf("case %d", idx), func(t *testing.T) {
+			base := NewSignal(tt.base)
+			exclude := NewSignal(tt.exclude)
+			expected := NewSignal(tt.expected)
+
+			actual := base.Exclude(exclude)
+
+			assert.Equal(t, expected, actual)
+		})
+	}
 }
