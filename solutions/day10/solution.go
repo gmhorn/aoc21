1a750760f2fe28ee91eaf889b46ed302dae5c071
Gerald Horn <geraldmhorn@gmail.com>
2021-12-28 16:11:25 -0800

    Part 1

--- solutions/day10/solution.go
+++ solutions/day10/solution.go
@@ -1,13 +1,62 @@
 package day10
 
-import "errors"
+import (
+	"errors"
+	"strings"
+
+	"github.com/gmhorn/aoc21/lib"
+)
 
 type Solution struct{}
 
 func (sln Solution) Part1(input string) (int, error) {
-	return -1, errors.New("not implemented")
+	lines, err := lib.ReadLines(input)
+	if err != nil {
+		return -1, err
+	}
+
+	total := 0
+	for _, line := range lines {
+		total += score(line)
+	}
+	return total, nil
 }
 
 func (sln Solution) Part2(input string) (int, error) {
 	return -1, errors.New("not implemented")
+}
+
+const openers = "([{<"
+const closers = ")]}>"
+
+var openerFor = map[rune]rune{
+	')': '(',
+	']': '[',
+	'}': '{',
+	'>': '<',
+}
+
+var values = map[rune]int{
+	')': 3,
+	']': 57,
+	'}': 1197,
+	'>': 25137,
+}
+
+// score returns the s
+func score(line string) int {
+	stack := NewStack()
+	for _, char := range line {
+		switch {
+		case strings.ContainsRune(openers, char):
+			stack.Push(char)
+		case strings.ContainsRune(closers, char):
+			expect := openerFor[char]
+			actual, _ := stack.Pop()
+			if actual != expect {
+				return values[char]
+			}
+		}
+	}
+	return 0
 }
