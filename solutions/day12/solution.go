5a26dac4d4378d62ea142fd2eef9263ffcdbe66f
Gerald Horn <geraldmhorn@gmail.com>
2021-12-30 21:19:38 -0800

    Part 2

--- solutions/day12/solution.go
+++ solutions/day12/solution.go
@@ -1,10 +1,19 @@
 package day12
 
 import (
-	"errors"
 	"strings"
 
 	"github.com/gmhorn/aoc21/lib"
+)
+
+type cave string
+type path []cave
+type graph map[cave][]cave
+type rule func(path, cave) bool
+
+const (
+	start cave = "start"
+	end   cave = "end"
 )
 
 type Solution struct{}
@@ -15,32 +24,74 @@ func (sln Solution) Part1(input string) (int, error) {
 		return -1, err
 	}
 
-	return countPaths(g, cave("start"), cave("end")), nil
+	return len(explore(g, visitOnce)), nil
 }
 
 func (sln Solution) Part2(input string) (int, error) {
-	return -1, errors.New("not implemented")
+	g, err := loadGraph(input)
+	if err != nil {
+		return -1, err
+	}
+
+	return len(explore(g, visitTwice)), nil
 }
 
-type cave string
-type path []cave
-type graph map[cave][]cave
+var visitOnce rule = func(p path, c cave) bool {
+	// big caves always get explored
+	if c.isBig() {
+		return true
+	}
 
-func (c cave) isSmall() bool {
-	return strings.ToLower(string(c)) == string(c)
+	// if c is already in the path, don't visit
+	for _, elem := range p {
+		if elem == c {
+			return false
+		}
+	}
+	return true
+}
+
+var visitTwice rule = func(p path, c cave) bool {
+	// never return to start
+	if len(p) > 0 && c == start {
+		return false
+	}
+
+	// big caves always get explored
+	if c.isBig() {
+		return true
+	}
+
+	// Assuming we do explore c, count how many times each small cave would be
+	// explored. If there are 2 or more greater than 2, don't explore
+	counts := make(map[cave]int)
+	for _, elem := range p {
+		if !elem.isBig() {
+			counts[elem]++
+		}
+	}
+	counts[c]++
+
+	cavesExploredTwice := 0
+	for _, cnt := range counts {
+		// don't explore any small cave more than twice
+		if cnt > 2 {
+			return false
+		}
+		// keep track of number of caves explored exactly twice
+		if cnt == 2 {
+			cavesExploredTwice++
+		}
+	}
+	return cavesExploredTwice < 2
+}
+
+func (c cave) isBig() bool {
+	return strings.ToUpper(string(c)) == string(c)
 }
 
 func (p path) last() cave {
 	return p[len(p)-1]
-}
-
-func (p path) contains(c cave) bool {
-	for _, elem := range p {
-		if c == elem {
-			return true
-		}
-	}
-	return false
 }
 
 func (p path) concat(c cave) path {
@@ -55,7 +106,7 @@ func (p path) String() string {
 	for _, c := range p {
 		parts = append(parts, string(c))
 	}
-	return strings.Join(parts, "-")
+	return strings.Join(parts, ",")
 }
 
 func loadGraph(input string) (graph, error) {
@@ -76,8 +127,8 @@ func loadGraph(input string) (graph, error) {
 	return g, nil
 }
 
-func countPaths(graph graph, start, end cave) int {
-	total := 0
+func explore(g graph, shouldExplore rule) []path {
+	paths := make([]path, 0)
 
 	toExplore := make([]path, 0)
 	toExplore = append(toExplore, []cave{start})
@@ -89,22 +140,19 @@ func countPaths(graph graph, start, end cave) int {
 
 		// if path terminates, increment the total and continue
 		if p.last() == end {
-			total++
+			paths = append(paths, p)
 			continue
 		}
 
 		// otherwise get all caves connected to the last cave in the path
-		for _, c := range graph[p.last()] {
-			// if the cave is small and we've already visited on this path, its
-			// a dead end
-			if c.isSmall() && p.contains(c) {
-				continue
+		for _, c := range g[p.last()] {
+			// if explore rule determines we should keep exploring, add to our
+			// list of paths to keep exploring.
+			if shouldExplore(p, c) {
+				toExplore = append(toExplore, p.concat(c))
 			}
-
-			// otherwise, add it to our list of paths to keep exploring
-			toExplore = append(toExplore, p.concat(c))
 		}
 	}
 
-	return total
+	return paths
 }
--- solutions/day12/solution_test.go
+++ solutions/day12/solution_test.go
@@ -1,17 +1,52 @@
 package day12
 
 import (
+	"io/ioutil"
+	"sort"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCountPaths(t *testing.T) {
-	g, err := loadGraph("testdata.txt")
-	assert.NoError(t, err)
+func TestExplore(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		rule     rule
+		expected string
+	}{{
+		name:     "visit once",
+		input:    "testdata/input.txt",
+		rule:     visitOnce,
+		expected: "testdata/visitOnce.txt",
+	}, {
+		name:     "visit twice",
+		input:    "testdata/input.txt",
+		rule:     visitTwice,
+		expected: "testdata/visitTwice.txt",
+	}}
 
-	actual := countPaths(g, cave("start"), cave("end"))
-	expected := 10
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g, err := loadGraph(tt.input)
+			assert.NoError(t, err)
 
-	assert.Equal(t, expected, actual)
+			paths := make([]string, 0)
+			for _, p := range explore(g, tt.rule) {
+				paths = append(paths, p.String())
+			}
+
+			sort.Slice(paths, func(i, j int) bool {
+				return paths[i] < paths[j]
+			})
+
+			actual := strings.Join(paths, "\n")
+
+			expected, err := ioutil.ReadFile(tt.expected)
+			assert.NoError(t, err)
+
+			assert.Equal(t, string(expected), actual)
+		})
+	}
 }
